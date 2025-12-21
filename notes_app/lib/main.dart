87d15ba4336b35f65d8699001def8593653955f2
Mariam Elbeshbeeshy <mariomaelsaeed2@gmail.com>
2025-12-21 03:58:29 +0200

    consume "Add note cubit" in Add Note Bottom Sheet

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/helper/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
@@ -17,57 +19,62 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: kDarkBackgroundColor,
-        appBarTheme: AppBarTheme(
-          backgroundColor: kDarkBackgroundColor,
-          foregroundColor: Colors.white,
-        ),
-        fontFamily: 'Poppins',
-        inputDecorationTheme: InputDecorationTheme(
-          contentPadding: EdgeInsets.all(16.0),
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: Colors.red),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context)=>AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+      
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: kDarkBackgroundColor,
+          appBarTheme: AppBarTheme(
+            backgroundColor: kDarkBackgroundColor,
+            foregroundColor: Colors.white,
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: Colors.white),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: kPrimaryColor),
-          ),
-          hintStyle: TextStyle(color: Colors.grey[400]),
-        ),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: kPrimaryColor,
-        ),
-        textTheme: const TextTheme(titleMedium: TextStyle(color: Colors.black)),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            textStyle: const TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.normal,
+          fontFamily: 'Poppins',
+          inputDecorationTheme: InputDecorationTheme(
+            contentPadding: EdgeInsets.all(16.0),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8.0)),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide(color: Colors.red),
             ),
-            backgroundColor: kPrimaryColor,
-            foregroundColor: kSecondaryColor,
-            minimumSize: const Size.fromHeight(50),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide.none,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.0),
+              borderSide: BorderSide(color: kPrimaryColor),
+            ),
+            hintStyle: TextStyle(color: Colors.grey[400]),
+          ),
+          textSelectionTheme: const TextSelectionThemeData(
+            cursorColor: kPrimaryColor,
+          ),
+          textTheme: const TextTheme(titleMedium: TextStyle(color: Colors.black)),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+              textStyle: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.normal,
+              ),
+              backgroundColor: kPrimaryColor,
+              foregroundColor: kSecondaryColor,
+              minimumSize: const Size.fromHeight(50),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide.none,
+              ),
             ),
           ),
         ),
+        routes: {EditNoteView.id: (context) => const EditNoteView()},
+        home: Scaffold(body: Center(child: NotesView())),
       ),
-      routes: {EditNoteView.id: (context) => const EditNoteView()},
-      home: Scaffold(body: Center(child: NotesView())),
     );
   }
 }
--- notes_app/lib/widgets/add_note_bottom_sheet.dart
+++ notes_app/lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,14 +11,26 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 16.0),
-      child: const SizedBox(
+      child: SizedBox(
         height: 500,
         child: SingleChildScrollView(
-          child: AddNoteForm(),
+          child: BlocConsumer(
+            builder: (context, state) {
+              return ModalProgressHUD(
+                inAsyncCall: state is AddNoteLoading ? true : false,
+                child: AddNoteForm(),
+              );
+            },
+            listener: (context, state) {
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
+              } else if (state is AddNoteFailure) {
+                print('failed: ${state.errMssg}');
+              }
+            },
+          ),
         ),
       ),
     );
   }
 }
-
-
